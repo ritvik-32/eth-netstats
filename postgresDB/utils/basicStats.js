263392f9c178086b8b83e6a85e71f732032552dc
0xmuralik <krishna192reddy@gmail.com>
2022-10-20 11:27:22 +0530

    consoles

--- postgresDB/utils/basicStats.js
+++ postgresDB/utils/basicStats.js
@@ -1,13 +1,28 @@
 const {pool}=require("../db")
 
 function createTable(){    
-    pool.query("CREATE TABLE basicStats(sessionguid UUID NOT NULL, created text NOT NULL, sessionlife integer NOT NULL)", 
+    // {
+    //     active: true,
+    //     syncing: false,
+    //     mining: false,
+    //     hashrate: 0,
+    //     peers: 8,
+    //     gasPrice: 2268045429,
+    //     uptime: 100
+    //   }
+      
+    pool.query("CREATE TABLE [IF NOT EXISTS] basicStats(active BOOLEAN NOT NULL, syncing BOOLEAN NOT NULL, mining BOOLEAN NOT NULL, hashrate NUMERIC NOT NULL, peers INT NOT NULL CHECK (peers >= 0), gasPrice BIGINT NOT NULL, uptime BIGINT NOT NULL CHECK (uptime >= 0))", 
         (err, res) => {
         console.log(err, res);
         pool.end();
     });
 }
 
+function insertTable(){
+
+}
+
 module.exports={
-    createTable: createTable
+    createTable: createTable,
+    insertTable: insertTable
 };
