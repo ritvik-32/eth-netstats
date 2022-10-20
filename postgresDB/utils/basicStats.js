6870bb27422d425e50e18db4d8388f233382cb02
0xmuralik <krishna192reddy@gmail.com>
2022-10-20 12:21:06 +0530

    debug

--- postgresDB/utils/basicStats.js
+++ postgresDB/utils/basicStats.js
@@ -11,7 +11,7 @@ function createTable(){
     //     uptime: 100
     //   }
       
-    pool.query("CREATE TABLE [IF NOT EXISTS] basicStats(active BOOLEAN NOT NULL, syncing BOOLEAN NOT NULL, mining BOOLEAN NOT NULL, hashrate NUMERIC NOT NULL, peers INT NOT NULL CHECK (peers >= 0), gasPrice BIGINT NOT NULL, uptime BIGINT NOT NULL CHECK (uptime >= 0))", 
+    pool.query("CREATE TABLE basicStats(active BOOLEAN NOT NULL, syncing BOOLEAN NOT NULL, mining BOOLEAN NOT NULL, hashrate NUMERIC NOT NULL, peers INT NOT NULL CHECK (peers >= 0), gasPrice BIGINT NOT NULL, uptime BIGINT NOT NULL CHECK (uptime >= 0))", 
         (err, res) => {
         console.log(err, res);
         // pool.end();
